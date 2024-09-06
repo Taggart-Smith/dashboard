ed5565254bf2ee2d18749fe6b5b8c2e3ab15dede
Taggart Smith <taggart.smith@gmail.com>
2024-09-06 09:42:43 -0600

    Created Carousel

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+    
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+
+    const carouselInner = document.querySelector('.carousel-inner');
+    const offset = -currentIndex * 100;
+    carouselInner.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
